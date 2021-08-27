10369b1940ec23745c7281c3cd42ad0211036dca
Robert Larkins <ayb4btu@gmail.com>
2021-08-27 21:30:48 +1200

    Add ApplicationAssemblyBeacon

---
+++ src/application/Company.Product.Application/ApplicationAssemblyBeacon.cs
@@ -0,0 +1,9 @@
+﻿namespace Company.Product.Application
+{
+    /// <summary>
+    /// Class to allow the Application Assembly to be retrieved. Used in Startup.cs.
+    /// </summary>
+    public class ApplicationAssemblyBeacon
+    {
+    }
+}
